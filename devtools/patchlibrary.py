f78369743e1c26daf00e827fa8b893b2648e96fd
fengchuan <fengchuanhn@gmail.com>
2022-09-10 00:10:02 +0800

    hh

--- devtools/patchlibrary.py
+++ devtools/patchlibrary.py
@@ -14,15 +14,14 @@ def patch()->None:
     files=os.listdir(patchdir)
     print('beginpatch')
     for filename in files:
-        content=None
         try:
             with open(os.path.join(patchdir,filename),'r',encoding='utf8') as tmpf:
                 filepath=tmpf.readline().strip()
 
                 content=tmpf.read()
                 realpath=filepath.replace('venv/Lib/site-packages',targetdir)
-                print(f'patchfile:     {realpath}')
-                with open(realpath,'w',encoding='utf8') as tf:
-                    tf.write(content)
+                if os.path.exists(realpath):
+                    with open(realpath,'w',encoding='utf8') as tf:
+                        tf.write(content)
         except Exception as e:
             print(e)
