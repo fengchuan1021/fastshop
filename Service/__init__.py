f45d1fe0be49273bb9e8eeec3bdde09aa118e3c0
fengchuan <fengchuanhn@gmail.com>
2022-09-09 08:46:06 +0800

    sss

--- Service/__init__.py
+++ Service/__init__.py
@@ -15,11 +15,10 @@ def getModelname(name):
     return name[0].upper()+name[1:].replace('Service', '')
 
 def __getattr__(name: str) -> Any:
-    for annotationname,classtype in thismodule.__annotations__.items():
-        if annotationname==name:
-            tmpinstance = classtype(getattr(Models, getModelname(name)))
-            setattr(thismodule, name, tmpinstance)
-            return tmpinstance
+    if classType:=thismodule.__annotations__.get(name,None):
+        tmpinstance = classType(getattr(Models, getModelname(name)))
+        setattr(thismodule, name, tmpinstance)
+        return tmpinstance
     if hasattr(Models, getModelname(name)):
         model = getattr(Models, getModelname(name))
         tmpinstance = CRUDBase(model)
