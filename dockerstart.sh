30d91c92f92d696a0e4d121d6524627f2551455d
fengchuan <fengchuanhn@gmail.com>
2022-09-10 09:55:46 +0800

    hh

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 if [ -z "$MODE" ] ; then
-  MODE="PROD"
+  MODE="main"
 fi
 
 if [ -z "$CMD" ] ; then
   CMD="APP"
 fi
+CPUNUMBER=`grep -c ^processor /proc/cpuinfo`
 if [ "$CMD" = "APP" ] ; then
-  if [ "$MODE" = "DEV" ] ; then
+  if [ "$MODE" = "dev" ] ; then
     uvicorn app:app --reload
-  elif [ "$MODE" = "PROD" ] ; then
-    CPUNUMBER=`grep -c ^processor /proc/cpuinfo`
-    WORKERS=`expr $CPUNUMBER \* 2`
-    gunicorn app:app --workers $WORKERS --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+  elif [ "$MODE" = "main" ] ; then
+
+    gunicorn app:app --workers $CPUNUMBER --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
   else
     uvicorn app:app --reload
   fi
-elif [ "$CMD" = "CELERYWORKER" ] ; then
+elif [ "$CMD" = "CELERY" ] ; then
     celery -A celery_mainworker beat -S redbeat.RedBeatScheduler -l info >/dev/null &
-    celery -A celery_mainworker worker --concurrency=4  --beat -S redbeat.RedBeatScheduler -l info
+    celery -A celery_mainworker worker --concurrency=$CPUNUMBER  --beat -S redbeat.RedBeatScheduler -l info
 fi
