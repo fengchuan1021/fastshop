570087b9195d98b7201da9e4a17cadc46bdf9de4
fengchuan <fengchuanhn@gmail.com>
2022-09-12 09:54:36 +0800

    two stage build.

--- Models/productModels/Product.py
+++ Models/productModels/Product.py
@@ -1,11 +1,18 @@
 
-
+from sqlalchemy.orm import deferred
 from Models.ModelBase import Base
 from sqlalchemy import Column, DateTime, Float, ForeignKey, text
-from sqlalchemy.dialects.mysql import BIGINT, DATETIME, ENUM, INTEGER, VARCHAR
+from sqlalchemy.dialects.mysql import BIGINT, DATETIME, ENUM, INTEGER, VARCHAR,TEXT
 class Product(Base):
     __tablename__ = 'product'
-    productName=Column(VARCHAR(256),nullable=True)
+
+    productName_en= deferred(Column(VARCHAR(255),nullable=True), group='en')
+    productDescription_en=deferred(Column(TEXT(),nullable=True), group='en')
+    brand_en=deferred(Column(VARCHAR(24),nullable=True), group='en')
+
+    productName_cn= deferred(Column(VARCHAR(255),nullable=True), group='cn')
+    productDescription_cn=deferred(Column(TEXT(),nullable=True), group='cn')
+    brand_cn=deferred(Column(VARCHAR(24),nullable=True), group='cn')
 
 class Category(Base):
     __tablename__ = 'cagetory'
