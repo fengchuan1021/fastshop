1bb176cdb94e27f1a1fbfe6f0d8789c40db9332d
fengchuan <fengchuanhn@gmail.com>
2022-09-15 01:08:02 +0800

    two stage build.

--- dockerstart.sh
+++ dockerstart.sh
@@ -9,11 +9,11 @@ fi
 CPUNUMBER=`grep -c ^processor /proc/cpuinfo`
 if [ "$CMD" = "APP" ] ; then
   if [ "$MODE" = "dev" ] ; then
-    uvicorn app:app --reload --port 80
+    uvicorn app:app --reload --port 80 --host=0.0.0.0
   elif [ "$MODE" = "main" ] ; then
     gunicorn app:app --workers $CPUNUMBER --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
   else
-    uvicorn app:app --reload --port 80
+    uvicorn app:app --reload --port 80 --host=0.0.0.0
   fi
 elif [ "$CMD" = "CELERY" ] ; then
     celery -A celery_mainworker beat -S redbeat.RedBeatScheduler -l info >/dev/null &
