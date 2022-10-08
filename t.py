be8df8174356d5be75fd457b24305cadded5ea14
fengchuan <fengchuanhn@gmail.com>
2022-10-08 16:55:13 +0800

    gitlab

--- t.py
+++ t.py
@@ -3,12 +3,13 @@ import Service
 from common.dbsession import getdbsession
 
 
-async def test():
+async def test()->None:
     db=await getdbsession()
     model=await Service.warehouseService.findByPk(db,81175036077016130)
 
 
-    model.warehouse_name='12331244'
+    model.warehouse_name='12331244d'
 
     await db.commit()
 asyncio.run(test())
+print('123')
