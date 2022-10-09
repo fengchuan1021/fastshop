794616dc75b610fd1ac4147a938cce72d2a6d015
fengchuan <fengchuanhn@gmail.com>
2022-10-09 19:30:50 +0800

    gitlab

--- dockerstart.sh
+++ dockerstart.sh
@@ -15,11 +15,10 @@ if [ "$CMD" = "APP" ] ; then
     gunicorn app:app --workers $CPUNUMBER --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
   else
     mkdir -p /app/alembic/versions
-    alembic revision --autogenerate -m "auto"
-    alembic upgrade head
+    python manage.py resetdb
     uvicorn app:app --reload --port 8000 --host=0.0.0.0
   fi
 elif [ "$CMD" = "CELERY" ] ; then
-    celery -A celery_mainworker beat -S redbeat.RedBeatScheduler -l info >/dev/null &
+    #celery -A celery_mainworker beat -S redbeat.RedBeatScheduler -l info >/dev/null &
     celery -A celery_mainworker worker --concurrency=$CPUNUMBER  --beat -S redbeat.RedBeatScheduler -l info
 fi
