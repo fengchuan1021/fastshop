e10f9f62c70ebcabc111ecd6665bd7dabeb684be
fengchuan <fengchuanhn@gmail.com>
2022-11-08 10:59:49 +0800

    gitlab

--- Service/thirdpartmarket/ThirdMarketService.py
+++ Service/thirdpartmarket/ThirdMarketService.py
@@ -4,16 +4,14 @@ import importlib
 from pathlib import Path
 from .__init__ import Market
 class ThirdMarketService():
-    pass
     def __init__(self)->None:
         self.markets={}
         files=os.listdir('market')
         for f in files:
-            if f.endswith('Market.py'):
-                print(Path(__file__).parent.joinpath('market',f[0:-3]).relative_to(settings.BASE_DIR).__str__().replace('\\','.'))
+            if f.endswith('Service.py'):
                 clsfile=importlib.import_module(Path(__file__).parent.joinpath('market',f[0:-3]).relative_to(settings.BASE_DIR).__str__().replace('\\','.'))
                 cls=getattr(clsfile,f[0:-3])
-                self.markets[f[0:-9].lower()]=cls
+                self.markets[f[0:-10].lower()]=cls
     async def getMarket(self,marketname:str)->Market:
         if marketname in self.markets:
             return self.markets[marketname]
