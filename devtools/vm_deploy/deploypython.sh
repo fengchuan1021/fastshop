4b25cb5b1fc8590a8e587ef36c2d34dbc56e8db4
fengchuan <fengchuanhn@gmail.com>
2022-11-13 20:01:21 +0800

    gitlab

--- devtools/vm_deploy/deploypython.sh
+++ devtools/vm_deploy/deploypython.sh
@@ -1,3 +1,6 @@
 sudo docker build . -t xtpythonimg --build-arg MODE=STAGE
 sudo  docker ps | grep xtpython && sudo docker stop xtpython
-sudo docker run -d --rm --name xtpython xtpythonimg
+sudo docker run -d --rm -p8000:8000 -e MODE="STAGE" -e CMD="APP" --name xtpython xtpythonimg
+
+sudo  docker ps | grep xtpythoncelery && sudo docker stop xtpythoncelery
+sudo docker run -d --rm  -e MODE="STAGE" -e CMD="CELERY" --name xtpythoncelery xtpythonimg
