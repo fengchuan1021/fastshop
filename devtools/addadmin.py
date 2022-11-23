7a259fd63d3ba7ee9790e074866f196991ccbc62
fengchuan <fengchuanhn@gmail.com>
2022-11-23 13:24:18 +0800

    gitlab

--- devtools/addadmin.py
+++ devtools/addadmin.py
@@ -3,15 +3,21 @@ import asyncio
 import Models
 from component.dbsession import getdbsession
 import Service
+from component.snowFlakeId import snowFlack
+
 
 async def adddefaultadmin()->None:
     async with getdbsession() as db:
         try:
-            root = Models.User(username='root', password=Service.userService.get_password_hash('root'), userrole=1)
-            await Service.userService.create(db,root)#type: ignore
-
+            root = Models.User(username='root', password=Service.userService.get_password_hash('root'))
+            root.user_id=snowFlack.getId()
+            await Service.userService.create(db, root)  # type: ignore
+            role=await Service.roleService.create(db,{"role_name":"root",'note':"super user"})
+            await db.flush()
+            await Service.userroleService.create(db,{"user_id":root.user_id,'role_id':role.role_id,'role_name':'root'})
             await db.commit()
         except Exception as e:
+            print(e)
             await db.rollback()
 
 def addroot()->None:
