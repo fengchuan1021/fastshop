deb282111c1db8c989223788b96c57ab69034092
fengchuan <fengchuanhn@gmail.com>
2022-11-27 20:23:54 +0800

    gitlab

--- component/graphqlpermission.py
+++ component/graphqlpermission.py
@@ -1,11 +1,12 @@
-from typing import List,Any
+from typing import List,Any,Dict
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import Service
-from cachetools import cached, LRUCache, TTLCache
-from cachetools.keys import hashkey
-@cached(cache={}, key=lambda db,modelname,role,method: f"{modelname}_{role}_{method}")#type: ignore
+cache_dic:Dict[str,Any]={}
 async def getAuthorizedColumns(db:AsyncSession,modelname:str,role:int,method:str='read')->Any:
+    key=f'{modelname}_{role}_{method}'
+    if key in cache_dic:
+        return cache_dic[key]
     permission=await Service.graphpermissionService.findOne(db,filter={"model_name":modelname,"role_id":role})
     if not permission:
         if role==1:
@@ -14,5 +15,8 @@ async def getAuthorizedColumns(db:AsyncSession,modelname:str,role:int,method:str
             raise PermissionError(f"roleid:{role} not set the table {modelname} permission")
     columns=getattr(permission,f'{method}_columns')
     extra=getattr(permission,f'{method}_extra')
-    return columns.split(',') if columns else [],extra.split(',') if extra else []
+    result=columns.split(',') if columns else [],extra.split(',') if extra else []
+    cache_dic[key]=result
+    return result
+
 
