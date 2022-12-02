47c99b5bca5fd470de1f48d798ad44ea181d5fe4
fengchuan <fengchuanhn@gmail.com>
2022-12-02 14:53:21 +0800

    fastapi

--- devtools/vm_deploy/deploypython.sh
+++ devtools/vm_deploy/deploypython.sh
@@ -1,6 +1,6 @@
-sudo docker build . -t xtpythonimg --build-arg MODE=STAGE --build-arg USE_TUNA=1
-sudo  docker ps | grep xtpython && sudo docker stop xtpython
-sudo docker run -d --rm -p8000:8000 -e MODE="STAGE" -e CMD="APP" --name xtpython xtpythonimg
+docker build . -t xtpythonimg --build-arg MODE=STAGE --build-arg USE_TUNA=1
+docker ps | grep xtpython && docker stop xtpython
+docker run -d --rm -p8000:8000 -e MODE="STAGE" -e CMD="APP" --name xtpython xtpythonimg
 
-sudo  docker ps | grep xtpythoncelery && sudo docker stop xtpythoncelery
-sudo docker run -d --rm  -e MODE="STAGE" -e CMD="CELERY" --name xtpythoncelery xtpythonimg
+docker ps | grep xtpythoncelery && docker stop xtpythoncelery
+docker run -d --rm  -e MODE="STAGE" -e CMD="CELERY" --name xtpythoncelery xtpythonimg
