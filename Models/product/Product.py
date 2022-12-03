c29b759e392511b20645cee9a7548e817d35226c
seanyang715 <sean.yang@unineedgroup.com>
2022-12-03 15:14:03 +0800

    add wish_product model

--- Models/product/Product.py
+++ Models/product/Product.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import deferred, relationship, backref
 import enum
 from Models.ModelBase import Base, XTVARCHAR
 from sqlalchemy import Column
-from sqlalchemy.dialects.mysql import BIGINT, ENUM, INTEGER, TEXT, DECIMAL
+from sqlalchemy.dialects.mysql import BIGINT, ENUM, INTEGER, TEXT, DECIMAL, DATETIME
 from component.snowFlakeId import snowFlack
 from typing import TYPE_CHECKING, List
 from .Brand import Brand
@@ -204,3 +204,23 @@ class Variant(Base):
     #                                                        primaryjoin='foreign(VariantWarehouse.variant_id) ==Variant.variant_id',
     #                                                        back_populates='Variant', cascade=''
     #                                                        )
+
+
+class WishProduct(Base):
+    __tablename__ = 'wish_product'
+    wish_product_id = Column(BIGINT(20), primary_key=True, default=snowFlack.getId)
+    status = Column(
+        ENUM("PENDING_REVIEW", "APPROVED", "REJECTED", "REMOVED_BY_WISH", "REMOVED_BY_MERCHANT"),
+        server_default='PENDING_REVIEW', default='PENDING_REVIEW',
+        comment="待审查 / 通过 / 拒绝 / 被wish移除 / 被商家移除")
+    created_at = Column(DATETIME)
+    updated_at = Column(DATETIME)
+    description = Column(TEXT)
+    tags = Column(XTVARCHAR(512), nullable=True, comment="")
+    subcategory_id = Column(INTEGER, default=0, comment="")
+    num_saves = Column(INTEGER, default=0, comment="")
+    category_experience_eligibility = Column(ENUM("TURE", "FALSE"))
+    num_sold = Column(INTEGER, default=0, comment="")
+    parent_sku = Column(XTVARCHAR(512), nullable=True, comment="")
+    wish_id = Column(XTVARCHAR(256), nullable=True, comment="")
+    name = Column(XTVARCHAR(512), nullable=True, comment="")
