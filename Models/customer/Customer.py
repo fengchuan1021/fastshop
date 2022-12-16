2f360955a8f29ce4d9df085195879169999cbd88
fengchuan <fengchuanhn@gmail.com>
2022-12-16 16:50:15 +0800

    Customer.py

--- Models/customer/Customer.py
+++ Models/customer/Customer.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import relationship
+
 from Models.ModelBase import Base, XTVARCHAR
 from sqlalchemy import Column
 from sqlalchemy.dialects.mysql import BIGINT, ENUM, INTEGER, DECIMAL, DATETIME, DATE, TIMESTAMP, CHAR
@@ -5,7 +7,7 @@ from sqlalchemy.dialects.mysql import BIGINT, ENUM, INTEGER, DECIMAL, DATETIME, 
 from component.snowFlakeId import snowFlack
 from typing import TYPE_CHECKING,List
 if TYPE_CHECKING:
-    pass
+    from Models import Store
 
 
 class Customer(Base):
@@ -16,5 +18,14 @@ class Customer(Base):
     country=Column(XTVARCHAR(64))
     country_code=Column(CHAR(3),index=True)
     order_amount=Column(DECIMAL(10,4),default=0,server_default="0")#总下单金额 包含未付款的
-    total_paid=Column(DECIMAL(10, 4),default=0,server_default="0")
-    order_count=Column(INTEGER)
+
+    order_count=Column(INTEGER)#总下单数量 包含未付款的
+    total_paid = Column(DECIMAL(10, 4), default=0, server_default="0")
+    order_paid=Column(INTEGER,default=0, server_default="0")
+    refund_money=Column(DECIMAL(10, 4), default=0,server_default="0")
+    refund_ordercnt=Column(INTEGER,default=0, server_default="0")
+    last_ordertime=Column(DATETIME)
+    store_id=Column(BIGINT,index=True)
+    store_name=Column(XTVARCHAR(32))
+    Store:Store=relationship('Store',uselist=False,primaryjoin="foreign(Customer.store_id)==Store.store_id",cascade='')
+
