d26bfce51cf31bc42f39b1079544cc56f8de2d46
fengchuan <chuan.fengchuan@unineedgroup.com>
2022-12-30 11:33:52 +0800

    fastapi

--- devtools/patchlibrary.py
+++ devtools/patchlibrary.py
@@ -9,21 +9,22 @@ def patch()->None:
     for f in files:
         if f.endswith('site-packages'):
             targetdir=f
+        else:
+            continue
+        patchdir=str(Path(__file__).parent.joinpath('patchs'))
+        files=os.listdir(patchdir)
+        print('beginpatch')
+        for filename in files:
+            try:
+                with open(os.path.join(patchdir,filename),'r',encoding='utf8') as tmpf:
+                    filepath=tmpf.readline().strip()
 
-    patchdir=str(Path(__file__).parent.joinpath('patchs'))
-    files=os.listdir(patchdir)
-    print('beginpatch')
-    for filename in files:
-        try:
-            with open(os.path.join(patchdir,filename),'r',encoding='utf8') as tmpf:
-                filepath=tmpf.readline().strip()
-
-                content=tmpf.read()
-                realpath=filepath.replace('venv/Lib/site-packages',targetdir)
-                if os.path.exists(realpath):
-                    with open(realpath,'w',encoding='utf8') as tf:
-                        tf.write(content)
-        except Exception as e:
-            print(e)
+                    content=tmpf.read()
+                    realpath=filepath.replace('venv/Lib/site-packages',targetdir)
+                    if os.path.exists(realpath):
+                        with open(realpath,'w',encoding='utf8') as tf:
+                            tf.write(content)
+            except Exception as e:
+                print(e)
 if __name__ == '__main__':
     patch()
